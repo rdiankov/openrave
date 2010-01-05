6430cda238bbbec6b477cf192d8d95426ccb5342
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-01-05 05:52:51 +0000

    added optional updating to python grasping

--- python/examples/grasping.py
+++ python/examples/grasping.py
@@ -43,7 +43,7 @@ class Grasping(metaclass.AutoReloader):
         self._mkdir(os.path.join(self.env.GetHomeDirectory(),self.robot.GetKinematicsGeometryHash()))
         return os.path.join(self.env.GetHomeDirectory(),self.robot.GetKinematicsGeometryHash(),self.target.GetKinematicsGeometryHash()+'.grasp.pp')
 
-    def generateGraspSet(self,preshapes,standoffs,rolls,approachrays, graspingnoise=None,addSphereNorms=False):
+    def generateGraspSet(self,preshapes,standoffs,rolls,approachrays, graspingnoise=None,addSphereNorms=False,updateenv=True):
         N = approachrays.shape[0]
         approachgraphs = [self.env.plot3(points=approachrays[:,0:3],pointsize=5,colors=array((1,0,0))),
                           self.env.drawlinelist(points=reshape(c_[approachrays[:,0:3],approachrays[:,0:3]+0.005*approachrays[:,3:6]],(2*N,3)),linewidth=4,colors=array((1,0,0,0)))]
@@ -73,11 +73,12 @@ class Grasping(metaclass.AutoReloader):
             except ValueError:
                 print 'grasp failed'
                 continue
-            
-            self.robot.SetJointValues(finalconfig[0])
-            self.robot.SetTransform(finalconfig[1])
-            contactgraph = self.drawContacts(contacts)
-            self.env.UpdatePublishedBodies()
+
+            if updateenv:
+                self.robot.SetJointValues(finalconfig[0])
+                self.robot.SetTransform(finalconfig[1])
+                contactgraph = self.drawContacts(contacts)
+                self.env.UpdatePublishedBodies()
             grasp[self.graspindices.get('igrasptrans')] = reshape(transpose(finalconfig[1][0:3,0:4]),12)
             grasp[self.graspindices.get('iforceclosure')] = mindist
             if mindist > 1e-9:
