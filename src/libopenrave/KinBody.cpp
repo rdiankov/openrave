279ebd7dcba4a151463d2ba651cf593ed3aa5664
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-01-08 19:55:16 +0000

    fixed crash in attempting to access null report

--- src/libopenrave/KinBody.cpp
+++ src/libopenrave/KinBody.cpp
@@ -1624,11 +1624,13 @@ void KinBody::CalculateAngularVelocityJacobian(int index, std::vector<dReal>& vJ
 bool KinBody::CheckSelfCollision(CollisionReportPtr report) const
 {
     if( GetEnv()->CheckSelfCollision(shared_kinbody_const(), report) ) {
-        RAVELOG_VERBOSEA("Self collision: (%s:%s)x(%s:%s).\n",
-                         !!report->plink1?report->plink1->GetParent()->GetName().c_str():"",
-                         !!report->plink1?report->plink1->GetName().c_str():"",
-                         !!report->plink2?report->plink2->GetParent()->GetName().c_str():"",
-                         !!report->plink2?report->plink2->GetName().c_str():"");
+        if( !!report ) {
+            RAVELOG_VERBOSEA("Self collision: (%s:%s)x(%s:%s).\n",
+                             !!report->plink1?report->plink1->GetParent()->GetName().c_str():"",
+                             !!report->plink1?report->plink1->GetName().c_str():"",
+                             !!report->plink2?report->plink2->GetParent()->GetName().c_str():"",
+                             !!report->plink2?report->plink2->GetName().c_str():"");
+        }
         return true;
     }
     return false;
