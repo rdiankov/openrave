842c45154b277f77e00a0b277929bcb5b459b9dd
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-01-11 00:37:53 +0000

    removed __declspec flags from ann build in windows

--- 3rdparty/ann/include/ANN/ANN.h
+++ 3rdparty/ann/include/ANN/ANN.h
@@ -57,7 +57,7 @@
 #ifndef ANN_H
 #define ANN_H
 
-#ifdef WIN32
+#if defined(WIN32) && !defined(ANN_STATIC_LIBRARY)
   //----------------------------------------------------------------------
   // For Microsoft Visual C++, externally accessible symbols must be
   // explicitly indicated with DLL_API, which is somewhat like "extern."
