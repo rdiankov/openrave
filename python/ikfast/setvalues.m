6748d49bf6b5ff207be24b9a301b955de903f24d
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-01-13 22:05:18 +0000

    cleaned up old ikfast dir

--- python/ikfast/setvalues.m
+++
@@ -1,7 +0,0 @@
-function setvalues(robotid, values)
-    limits = orRobotGetDOFLimits(robotid);
-if( any(values'<limits(:,1)|values'>limits(:,2)) )
-    display('bad limits')
-    return
-end
-orBodySetJointValues(robotid,values,0:(length(values)-1))
