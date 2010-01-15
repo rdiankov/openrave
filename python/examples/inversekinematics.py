5a1eed5c23ef52393eb84faa65e8dcf5ee94ea23
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-01-15 03:24:50 +0000

    now inversekinematics works for python 2.5

--- python/examples/inversekinematics.py
+++ python/examples/inversekinematics.py
@@ -188,15 +188,15 @@ class InverseKinematicsModel(OpenRAVEModel):
         if not path:
             raise ValueError("no path specified")
 
-        start_list = os.path.abspath(start).split(sep)
-        path_list = os.path.abspath(path).split(sep)
+        start_list = os.path.abspath(start).split(os.path.sep)
+        path_list = os.path.abspath(path).split(os.path.sep)
 
         # Work out how much of the filepath is shared by start and path.
         i = len(os.path.commonprefix([start_list, path_list]))
 
-        rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
+        rel_list = [os.path.pardir] * (len(start_list)-i) + path_list[i:]
         if not rel_list:
-            return curdir
+            return os.path.curdir
         return os.path.join(*rel_list)
 
     @staticmethod
