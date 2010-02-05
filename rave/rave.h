f7b208aa9b7ed4a2027648d20db4366cb129d7a0
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2010-02-05 22:05:07 +0000

    remove msvc language encoding warnings

--- rave/rave.h
+++ rave/rave.h
@@ -56,6 +56,7 @@
 
 #pragma warning(disable:4251) // needs to have dll-interface to be used by clients of class
 #pragma warning(disable:4190) // C-linkage specified, but returns UDT 'boost::shared_ptr<T>' which is incompatible with C
+#pragma warning(disable:4819) //The file contains a character that cannot be represented in the current code page (932). Save the file in Unicode format to prevent data loss using native typeof
 
 // needed to get typeof working
 //#include <boost/typeof/std/string.hpp>
