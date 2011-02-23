f9d12075f1e674767f89a83f3a7d8f6f41a45510
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2011-02-23 14:28:29 +0000

    test scripts

---
+++ test/jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# this is the main script needed to be executed by jenkins
+# assuming the workspace has 'openrave' folder for sources and 'data' folder for scenes and robots.
+export PARALLEL_JOBS=-j4
+export OPENRAVE_DATA=`pwd`/data
+export OPENRAVE_DATABASE=`pwd`/.openrave
+mkdir -p .openrave
+cd openrave
+make test prefix=`pwd`/../localinstall
