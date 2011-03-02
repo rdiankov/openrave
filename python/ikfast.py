d8fa15b530923e503ed39405c93b4188f1722f06
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2011-03-02 02:47:55 +0000

    fixed one more ikfast CannotSolveError problem

--- python/ikfast.py
+++ python/ikfast.py
@@ -2385,7 +2385,7 @@ class IKFastSolver(AutoReloader):
         othersymbols = list(rawpolyeqs[0][1].symbols)
         symbolsubs = [(symbols[i].subs(self.invsubs),symbols[i]) for i in range(len(symbols))]
         if len(symbols) != 6:
-            raise CannotSolveError('Kohli/Osvatic method requires 3 unknown variables')
+            raise self.CannotSolveError('Kohli/Osvatic method requires 3 unknown variables')
             
         # choose which leftvar can determine the singularity of the following equations!
         for i in range(0,6,2):
