26039fc927e4ff823ac9271d8844ea6f5bf0963b
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2011-03-30 12:47:24 +0000

    docs updates

--- docs/sendtoserver.sh
+++ docs/sendtoserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tar cjf openravedocs-`openrave-config --version`.tgz build/en/coreapihtml build/en/main build/ja/coreapihtml build/ja/main 
-scp openravedocs-`openrave-config --version`.tgz diankov@programmingvision.com:~/openrave/openravedocs.tgz
+tar cjf openravedocs.tgz build/en/coreapihtml build/en/main build/ja/coreapihtml build/ja/main 
+scp openravedocs.tgz diankov@programmingvision.com:~/openrave/
 ssh diankov@programmingvision.com "cd ~/openrave; rm -rf build; tar xjf openravedocs.tgz; mv -f en en_old; mv -f ja ja_old; mv build/en .; mv build/ja .; rm -rf en_old ja_old openravedocs.tgz build"
