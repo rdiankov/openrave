add871c348067494440c1068d9793021f6975b3b
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2011-06-21 15:53:54 +0000

    fixed test

--- test/test_environment.py
+++ test/test_environment.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2011 Rosen Diankov <rosen.diankov@gmail.com>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,4 +18,4 @@ _multiprocess_can_split_ = True
 class TestEnvironment(EnvironmentSetup):
     def test_load(self):
         env=self.env
-        assert(env.Load('../src/models/WAM/wam0.iv'))
+        env.Load('../src/models/WAM/wam0.iv')
