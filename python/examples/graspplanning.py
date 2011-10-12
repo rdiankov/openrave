0735eafb3e77fd04b43682e36b7d26db7b1efa74
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2011-10-12 04:31:59 +0000

    graspplanning minor change

--- python/examples/graspplanning.py
+++ python/examples/graspplanning.py
@@ -395,7 +395,7 @@ class GraspPlanning:
                     graspables = self.graspables[:]
                 else:
                     break
-            i = 4#random.randint(len(graspables))
+            random.randint(len(graspables))
             try:
                 print 'grasping object %s'%graspables[i][0].target.GetName()
                 with self.envreal:
