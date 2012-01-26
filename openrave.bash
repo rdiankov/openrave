31407084fa907069e85b04e2b0524f6244836f0c
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2012-01-26 06:35:39 +0000

    bash/ik fixes stuff

--- openrave.bash
+++ openrave.bash
@@ -4,7 +4,8 @@ if test $# -eq 0; then
     echo 'need a path'
 else
     export PATH=$1/bin:$PATH
-    export PYTHONPATH=`openrave-config --python-dir`:$PYTHONPATH
     export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
+    export PYTHONPATH=`openrave-config --python-dir`:$PYTHONPATH
+    export OCTAVE_PATH=`openrave-config --octave-dir`:$OCTAVE_PATH
     source `openrave-config --share-dir`/openrave_completion.bash
 fi
