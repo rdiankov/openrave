19ba1e31abcb20c8c4fd1fc008b8d7b5a3e811b8
openravetesting <openravetesting@9e016c41-b246-0410-9d68-e1838aa46861>
2012-06-06 10:46:26 +0000

    fixed test case

--- plugins/rmanipulation/basemanipulation.cpp
+++ plugins/rmanipulation/basemanipulation.cpp
@@ -292,6 +292,11 @@ protected:
             }
         }
 
+        if( maxsteps <= 0 ) {
+            RAVELOG_WARN("maxsteps is not positive\n");
+            return false;
+        }
+
         params->minimumcompletetime = params->_fStepLength * minsteps;
         RAVELOG_DEBUG(str(boost::format("Starting MoveHandStraight dir=(%f,%f,%f)...")%direction.x%direction.y%direction.z));
         robot->RegrabAll();
