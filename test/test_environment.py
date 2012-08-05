d2d8f387989a8624ea1b880b516decab098c5a70
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2012-08-05 11:47:41 +0000

    fixed test

--- test/test_environment.py
+++ test/test_environment.py
@@ -355,7 +355,7 @@ class TestEnvironment(EnvironmentSetup):
             clonedenv.Clone(env, CloningOptions.Bodies)
             endtime=time.time()-starttime
             self.log.info('new clone time: %fs',endtime)
-            assert(endtime <= 0.04)
+            assert(endtime <= 0.05)
             self.CompareEnvironments(env,clonedenv)
 
             env.Remove(env.GetKinBody('mug1'))
@@ -373,7 +373,7 @@ class TestEnvironment(EnvironmentSetup):
             clonedenv.Clone(env, CloningOptions.Bodies)
             endtime=time.time()-starttime
             self.log.info('new clone time: %fs',endtime)
-            assert(endtime <= 0.04)
+            assert(endtime <= 0.05)
             self.CompareEnvironments(env,clonedenv)
             
     def test_multithread(self):
