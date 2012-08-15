42fa2a3b9752838c67597e72440420d616800735
rdiankov <rdiankov@9e016c41-b246-0410-9d68-e1838aa46861>
2012-08-15 12:03:15 +0000

    fixed example

--- docs/source/tutorials/openravepy_examples/scene_logging.py
+++ docs/source/tutorials/openravepy_examples/scene_logging.py
@@ -4,4 +4,4 @@ from openravepy import *
 env = Environment() # create openrave environment
 env.Load('data/lab1.env.xml') # load a simple scene
 # save in a zip archive
-env.Save('newlab.zae',Environment.SelectOptions.Everything)
+env.Save('newlab.zae',Environment.SelectionOptions.Everything)
