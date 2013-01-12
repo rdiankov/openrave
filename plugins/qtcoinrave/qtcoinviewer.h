1877e8228ffe366101739af52c728d8cb653be22
Robert Ellenberg <rwe24@drexel.edu>
2013-01-12 03:23:54 -0500

    Added custom video frame size to qtcoin
    Replaced VIDEO_WIDTH and VIDEO_HEIGHT with variables in qtcoin class so
    that the rendered frame size can be adjusted.

--- plugins/qtcoinrave/qtcoinviewer.h
+++ plugins/qtcoinrave/qtcoinviewer.h
@@ -339,6 +339,7 @@ public:
 
     bool _SetFiguresInCamera(ostream& sout, istream& sinput);
     bool _SetFeedbackVisibility(ostream& sout, istream& sinput);
+    bool _CommandResize(ostream& sout, istream& sinput);
 
     // selection and deselection handling
     static void _SelectHandler(void *, class SoPath *);
@@ -459,6 +460,9 @@ public:
     std::list<UserDataWeakPtr> _listRegisteredItemSelectionCallbacks;
     std::list<UserDataWeakPtr> _listRegisteredViewerImageCallbacks;
     std::list<UserDataWeakPtr> _listRegisteredViewerThreadCallbacks;
+
+    unsigned int _width;
+    unsigned int _height;
 
     friend class EnvMessage;
     friend class ViewerSetSizeMessage;
