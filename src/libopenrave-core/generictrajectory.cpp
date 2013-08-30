cc732ecdd922de84b5c3eb9ae5e9fb3218360fdd
rdiankov <rosen.diankov@gmail.com>
2013-08-31 00:11:31 +0900

    minor threshold fix

--- src/libopenrave-core/generictrajectory.cpp
+++ src/libopenrave-core/generictrajectory.cpp
@@ -22,7 +22,7 @@
 namespace OpenRAVE {
 
 static const dReal g_fEpsilonLinear = RavePow(g_fEpsilon,0.9);
-static const dReal g_fEpsilonQuadratic = RavePow(g_fEpsilon,0.55); // should be 0.6...perhaps this is related to parabolic smoother epsilons?
+static const dReal g_fEpsilonQuadratic = RavePow(g_fEpsilon,0.5); // should be 0.6...perhaps this is related to parabolic smoother epsilons?
 
 class GenericTrajectory : public TrajectoryBase
 {
