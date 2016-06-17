4d8a7f08dc975f01d104d4f531df3a69ebe9523b
Ziyan Zhou <ziyan.zhou@mujin.co.jp>
2016-06-17 10:40:48 +0900

    Rosen fix fcl assert that's blocking progress.

--- plugins/fclrave/fclcollision.h
+++ plugins/fclrave/fclcollision.h
@@ -460,7 +460,9 @@ private:
 
         _fclspace->Synchronize(plink->GetParent());
         FOREACH(itbody, attachedBodies) {
-            _fclspace->Synchronize(*itbody);
+            if( (*itbody)->GetEnvironmentId() ) { // for now GetAttached can hold bodies that are not initialized
+                _fclspace->Synchronize(*itbody);
+            }
         }
 
         TemporaryManager tmpManagerBodyLink(_fclspace);
@@ -503,7 +505,9 @@ private:
         plinkParent->GetAttached(excludedBodies);
 
         FOREACH(itbody, vbodyexcluded) {
-            excludedBodies.insert(GetEnv()->GetBodyFromEnvironmentId((*itbody)->GetEnvironmentId()));
+            if( (*itbody)->GetEnvironmentId() ) { // for now GetAttached can hold bodies that are not initialized
+                excludedBodies.insert(GetEnv()->GetBodyFromEnvironmentId((*itbody)->GetEnvironmentId()));
+            }
         }
 
         FOREACH(itbody, excludedBodies) {
@@ -734,6 +738,9 @@ private:
                     if(*itbody == pbody) {
                         continue;
                     }
+                    if( (*itbody)->GetEnvironmentId() == 0 ) { // for now GetAttached can hold bodies that are not initialized
+                        continue;
+                    }
                     if( find(vbodyexcluded.begin(), vbodyexcluded.end(), *itbody) == vbodyexcluded.end() ) {
                         KinBody::LinkPtr pgrabbinglink = probot->IsGrabbing(*itbody);
                         if( !!pgrabbinglink && vactiveLinks[pgrabbinglink->GetIndex()]) {
@@ -761,16 +768,20 @@ private:
 
             if( vlinkexcluded.size() == 0 ) {
                 FOREACH(itbody, attachedBodies) {
-                    if( find(vbodyexcluded.begin(), vbodyexcluded.end(), *itbody) == vbodyexcluded.end() ) {
-                        tmpManagerBodyAgainstEnv.TransferRegistration(*itbody);
+                    if( (*itbody)->GetEnvironmentId() ) { // for now GetAttached can hold bodies that are not initialized
+                        if( find(vbodyexcluded.begin(), vbodyexcluded.end(), *itbody) == vbodyexcluded.end() ) {
+                            tmpManagerBodyAgainstEnv.TransferRegistration(*itbody);
+                        }
                     }
                 }
             } else {
                 FOREACH(itbody, attachedBodies) {
-                    if( find(vbodyexcluded.begin(), vbodyexcluded.end(), *itbody) == vbodyexcluded.end() ) {
-                        FOREACH(itlink, (*itbody)->GetLinks()) {
-                            if( find(vlinkexcluded.begin(), vlinkexcluded.end(), *itlink) == vlinkexcluded.end() ) {
-                                tmpManagerBodyAgainstEnv.TransferRegistration(*itlink);
+                    if( (*itbody)->GetEnvironmentId() ) { // for now GetAttached can hold bodies that are not initialized
+                        if( find(vbodyexcluded.begin(), vbodyexcluded.end(), *itbody) == vbodyexcluded.end() ) {
+                            FOREACH(itlink, (*itbody)->GetLinks()) {
+                                if( find(vlinkexcluded.begin(), vlinkexcluded.end(), *itlink) == vlinkexcluded.end() ) {
+                                    tmpManagerBodyAgainstEnv.TransferRegistration(*itlink);
+                                }
                             }
                         }
                     }
@@ -825,6 +836,9 @@ private:
                     if(*itbody == pbody) {
                         continue;
                     }
+                    if( (*itbody)->GetEnvironmentId() == 0 ) { // for now GetAttached can hold bodies that are not initialized
+                        continue;
+                    }
                     KinBody::LinkPtr pgrabbinglink = probot->IsGrabbing(*itbody);
                     if( !!pgrabbinglink && vactiveLinks[pgrabbinglink->GetIndex()]) {
                         tmpManager.Register(*itbody, first);
@@ -837,7 +851,9 @@ private:
             std::set<KinBodyPtr> attachedBodies;
             pbody->GetAttached(attachedBodies);
             FOREACH(itbody, attachedBodies) {
-                tmpManager.Register(*itbody, first);
+                if( (*itbody)->GetEnvironmentId() ) { // for now GetAttached can hold bodies that are not initialized
+                    tmpManager.Register(*itbody, first);
+                }
             }
         }
     }
