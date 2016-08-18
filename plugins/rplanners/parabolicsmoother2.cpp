32ebb56d2079cc0a994e5ad623233ed9a59a05f7
Puttichai <Puttichai.Lertkultanon@mujin.co.jp>
2016-08-18 14:49:15 +0900

    modified authors

--- plugins/rplanners/parabolicsmoother2.cpp
+++ plugins/rplanners/parabolicsmoother2.cpp
@@ -1,5 +1,5 @@
 // -*- coding: utf-8 -*-
-// Copyright (C) 2016 Puttichai Lertkultanon <L.Puttichai@gmail.com>
+// Copyright (C) 2016 Puttichai Lertkultanon & Rosen DianKov
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the
 // GNU Lesser General Public License as published by the Free Software Foundation, either version 3
