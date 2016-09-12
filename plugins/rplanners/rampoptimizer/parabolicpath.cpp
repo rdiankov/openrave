6f9cd3885eada263bf3aee23023f3e9b0ee45089
Puttichai <Puttichai.Lertkultanon@mujin.co.jp>
2016-09-12 10:28:41 +0900

    removed unused Timer.h & Timer.cpp

--- plugins/rplanners/rampoptimizer/parabolicpath.cpp
+++ plugins/rplanners/rampoptimizer/parabolicpath.cpp
@@ -12,7 +12,6 @@
 // You should have received a copy of the GNU Lesser General Public License along with this program.
 // If not, see <http://www.gnu.org/licenses/>.
 #include "parabolicpath.h"
-#include "Timer.h"
 #include <algorithm>
 
 namespace OpenRAVE {
