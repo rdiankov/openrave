43aff3bf451a1ef1d9f7744486f2ab7f5db801de
RobertYe <robert.ye@mujin.co.jp>
2016-11-15 14:50:14 +0900

    add error msg

--- plugins/rmanipulation/visualfeedback.cpp
+++ plugins/rmanipulation/visualfeedback.cpp
@@ -289,6 +289,7 @@ public:
                 if( bOutputError ) {
                     errormsg = str(boost::format("{\"type\":\"outofcamera\""));
                 }
+                
                 return false;
             }
 
@@ -351,6 +352,7 @@ public:
                 OBB cameraobb = geometry::TransformOBB(tCameraInTargetinv,*itobb);
                 if( !SampleProjectedOBBWithTest(cameraobb, _vf->_fSampleRayDensity, boost::bind(&VisibilityConstraintFunction::_TestRay, this, _1, boost::ref(tworldcamera)),_vf->_fAllowableOcclusion) ) {
                     RAVELOG_VERBOSE("box is occluded\n");
+                    errormsg = str(boost::format("{\"type\":\"pattern occluded\""));
                     return true;
                 }
             }
