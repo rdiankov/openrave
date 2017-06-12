cd23886ba5af82df4c3e61af175c8d728bf7607a
Ziyan Zhou <ziyan.zhou@mujin.co.jp>
2017-06-12 16:16:13 +0900

    Disable initial show command, which seems to be causing XIOError.

--- plugins/qtosgrave/qtosgviewer.cpp
+++ plugins/qtosgrave/qtosgviewer.cpp
@@ -1206,7 +1206,7 @@ bool QtOSGViewer::_StartViewerLoopCommand(ostream& sout, istream& sinput)
     sinput >> bcallmain;
     _nQuitMainLoop = -1;
     //_StartPlaybackTimer();
-    this->show();
+    // this->show();
     if( bcallmain ) {
         _posgWidget->SetHome();
         QApplication::instance()->exec();
@@ -1244,11 +1244,11 @@ int QtOSGViewer::main(bool bShow)
     }
     _nQuitMainLoop = -1;
     //_StartPlaybackTimer();
-    if (bShow) {
-        if( _nQuitMainLoop < 0 ) {
-            this->show();
-        }
-    }
+    // if (bShow) {
+    //     if( _nQuitMainLoop < 0 ) {
+    //         this->show();
+    //     }
+    // }
 
     UpdateFromModel();
     _posgWidget->SetHome();
