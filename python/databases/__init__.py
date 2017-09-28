c85fc15eaf20a8e022ba75239aaa283a4942da13
Eddie G <eddie_g_89@hotmail.com>
2017-09-28 12:11:35 -0700

    fixed writing file

--- python/databases/__init__.py
+++ python/databases/__init__.py
@@ -108,7 +108,7 @@ class DatabaseGenerator(metaclass.AutoReloader):
             makedirs(os.path.split(filename)[0])            
         except OSError:
             pass
-        with open(filename, 'w') as f:
+        with open(filename, 'wb') as f:
             pickle.dump((self.getversion(),params), f)
     def generate(self):
         raise NotImplementedError()
