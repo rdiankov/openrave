91265f2afcdc42f08ebe1496274a7ce406f9fac6
rosen.diankov@gmail.com <rosen.diankov@mujin.co.jp>
2018-01-31 12:33:15 +0900

    print this

--- plugins/fclrave/fclmanagercache.h
+++ plugins/fclrave/fclmanagercache.h
@@ -289,7 +289,7 @@ public:
                         ssinfo += str(boost::format("id=%d, linkmask=0x%x, numcols=%d")%itcache->first%itcache->second.linkmask%itcache->second.vcolobjs.size());
                     }
                 }
-                RAVELOG_WARN_FORMAT("%u tracking body not in current cached bodies (tracking body %s (id=%d)) (env %d). Current cache is: %s", _lastSyncTimeStamp%ptrackingbody->GetName()%ptrackingbody->GetEnvironmentId()%ptrackingbody->GetEnv()->GetId()%ssinfo);
+                RAVELOG_WARN_FORMAT("%x tracking body not in current cached bodies (tracking body %s (id=%d)) (env %d). Current cache is: %s", this%_lastSyncTimeStamp%ptrackingbody->GetName()%ptrackingbody->GetEnvironmentId()%ptrackingbody->GetEnv()->GetId()%ssinfo);
             }
             else {
                 FCLSpace::KinBodyInfoPtr pinfo = ittracking->second.pwinfo.lock();
