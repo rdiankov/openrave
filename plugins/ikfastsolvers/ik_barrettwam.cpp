944c971077a248e913b6601f6f1812c746e4df4c
Woody Chow <woody.chow@mujin.co.jp>
2018-11-15 15:04:46 +0900

    Rename GetFreeParameters to GetFreeIndices in iksolver.h to eliminate inconsistency with GetFreeParameters of manipulator. It's a more appropriate function name anyways.
    Bump ikfast version to 0x1000004b

--- plugins/ikfastsolvers/ik_barrettwam.cpp
+++ plugins/ikfastsolvers/ik_barrettwam.cpp
@@ -26,7 +26,7 @@ using namespace ikfast;
 
 // check if the included ikfast version matches what this file was compiled with
 #define IKFAST_COMPILE_ASSERT(x) extern int __dummy[(int)x]
-IKFAST_COMPILE_ASSERT(IKFAST_VERSION==0x10000048);
+IKFAST_COMPILE_ASSERT(IKFAST_VERSION==0x1000004b);
 
 #include <cmath>
 #include <vector>
@@ -391,7 +391,7 @@ eetrans[2]=((0.346)+((x18*x6))+((x11*((((x24*x53))+((x17*x4*x7))))))+(((-0.3)*x2
 }
 
 IKFAST_API int GetNumFreeParameters() { return 1; }
-IKFAST_API int* GetFreeParameters() { static int freeparams[] = {2}; return freeparams; }
+IKFAST_API int* GetFreeIndices() { static int freeindices[] = {2}; return freeindices; }
 IKFAST_API int GetNumJoints() { return 7; }
 
 IKFAST_API int GetIkRealSize() { return sizeof(IkReal); }
@@ -20518,7 +20518,7 @@ IkSolverBasePtr CreateIkSolver(EnvironmentBasePtr penv, std::istream& sinput, co
     ikfunctions->_ComputeIk = IKFAST_NAMESPACE::ComputeIk;
     ikfunctions->_ComputeFk = IKFAST_NAMESPACE::ComputeFk;
     ikfunctions->_GetNumFreeParameters = IKFAST_NAMESPACE::GetNumFreeParameters;
-    ikfunctions->_GetFreeParameters = IKFAST_NAMESPACE::GetFreeParameters;
+    ikfunctions->_GetFreeIndices = IKFAST_NAMESPACE::GetFreeIndices;
     ikfunctions->_GetNumJoints = IKFAST_NAMESPACE::GetNumJoints;
     ikfunctions->_GetIkRealSize = IKFAST_NAMESPACE::GetIkRealSize;
     ikfunctions->_GetIkFastVersion = IKFAST_NAMESPACE::GetIkFastVersion;
