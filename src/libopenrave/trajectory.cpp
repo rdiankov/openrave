d712ac56ce69d6dfce4d36bd2d50889a0de3cddb
simon.zheng <simon.zheng@mujin.co.jp>
2020-04-15 21:31:17 +0900

    revert log changes

--- src/libopenrave/trajectory.cpp
+++ src/libopenrave/trajectory.cpp
@@ -65,7 +65,7 @@ void TrajectoryBase::deserialize(std::istream& I)
         I.seekg((size_t)pos+ppsize);
     }
     else {
-        throw OPENRAVE_EXCEPTION_FORMAT(_("error, failed to find </trajectory> in [%s]"), buf.str(), ORE_InvalidArguments);
+        throw OPENRAVE_EXCEPTION_FORMAT(_("error, failed to find </trajectory> in %s"), buf.str(), ORE_InvalidArguments);
     }
     xmlreaders::TrajectoryReader readerdata(GetEnv(),shared_trajectory());
     xmlreaders::ParseXMLData(readerdata, pbuf.c_str(), ppsize);
