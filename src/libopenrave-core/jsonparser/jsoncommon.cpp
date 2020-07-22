031905d24bbe57fa61531e156dca4a9b208664ab
simon.zheng <simon.zheng@mujin.co.jp>
2020-07-22 17:54:41 +0900

    format code

--- src/libopenrave-core/jsonparser/jsoncommon.cpp
+++ src/libopenrave-core/jsonparser/jsoncommon.cpp
@@ -14,5 +14,4 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #include "jsoncommon.h"
