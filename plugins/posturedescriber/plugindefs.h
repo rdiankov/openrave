9aaa8fc1e360b3847f1eb3c24940614380063725
simon.zheng <simon.zheng@mujin.co.jp>
2020-07-31 15:31:27 +0900

    add _isRobot to python binding

---
+++ plugins/posturedescriber/plugindefs.h
@@ -0,0 +1,24 @@
+// -*- coding: utf-8 -*-
+// Copyright (C) 2006-2020 Guangning Tan, Kei Usui, Rosen Diankov <rosen.diankov@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef PLUGINS_POSTUREDESCRIBER_PLUGINDEFS_H
+#define PLUGINS_POSTUREDESCRIBER_PLUGINDEFS_H
+
+#define POSTUREDESCRIBER_CLASS_NAME   "posturedescriber"  // PostureDescriber
+#define POSTUREDESCRIBER_MODULE_NAME  "posturedescriber" // PostureDescriberModule
+#define POSTUREDESCRIBER_STATE_NAME   "posturestates"  // key in std::map<std::string, std::vector<dReal> > IkReturn::_mapdata
+
+#endif // PLUGINS_POSTUREDESCRIBER_PLUGINDEFS_H
