rdiankov/openrave

commit 0537a3f3f5a585b70ceba1d4e911ae956a7787f8

syntax cleanup

src/libopenrave/kinbodyjoint.cpp
@@ -2003,11 +2003,11 @@ void KinBody::Joint::SetMimicEquations(int iaxis, const std::string& poseq, cons
 
     // need to set sVars to resultVars since that's what the user will be feeding with the input
     std::stringstream sVars;
-    if( !!nVars ) {
-        sVars << resultVars[0];
-        for(size_t i = 1; i < nVars; ++i) {
-            sVars << "," << resultVars[i];
+    for(size_t ivar = 0; ivar < resultVars.size(); ++ivar) {
+        if( ivar != 0 ) {
+            sVars << ",";
         }
+        sVars << resultVars[ivar];
     }
 
     for(int itype = 1; itype < 3; ++itype) {
@@ -2031,7 +2031,10 @@ void KinBody::Joint::SetMimicEquations(int iaxis, const std::string& poseq, cons
          */
         utils::SearchAndReplace(eq, pmimic->_equations[itype], jointnamepairs);
         size_t index = eq.find('|');
-        const std::string sCommonSubexpressions = (index != std::string::npos) ? eq.substr(0, index) : ""; ///< common subexpressions
+        std::string sCommonSubexpressions = (index != std::string::npos) ? eq.substr(0, index) : ""; ///< common subexpressions
+        if( !sCommonSubexpressions.empty() ) {
+            sCommonSubexpressions += ';'; // just in case, separate out the equations
+        }
 
         while(index != std::string::npos) {
             size_t startindex = index + 1;
@@ -2094,7 +2097,7 @@ void KinBody::Joint::_ComputePartialVelocities(std::vector<std::pair<int, dReal>
                                                const int iaxis,
                                                std::map< std::pair<Mimic::DOFFormat, int>, dReal >& mTotalderivativepairValue) const
 {
-    OPENRAVE_ASSERT_FORMAT(vDofindexDerivativePairs.empty(), "Expect empty vDofindexDerivativePairs; now it has size %d", vDofindexDerivativePairs.size(), ORE_InvalidArguments);
+    vDofindexDerivativePairs.clear();
     if( this->dofindex >= 0 ) {
         vDofindexDerivativePairs.emplace_back(this->dofindex + iaxis, 1.0); // this joint is active; return immediately
         return;
@@ -2118,25 +2121,18 @@ void KinBody::Joint::_ComputePartialVelocities(std::vector<std::pair<int, dReal>
         thisdofformat.jointindex = nActiveJoints + (find(begin(vPassiveJoints), end(vPassiveJoints), shared_from_this()) - begin(vPassiveJoints));
     }
 
-    const std::map< std::pair<Mimic::DOFFormat, int>, dReal >::const_iterator mit = find_if(begin(mTotalderivativepairValue), end(mTotalderivativepairValue),
-                                                                                            [&thisdofformat](const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal> &keyvalue) {
-            const bool bfound = keyvalue.first.first == thisdofformat;
-            if( IS_DEBUGLEVEL(Level_Verbose) && bfound ) {
+    FOREACH(itTotalDeriv, mTotalderivativepairValue) {
+        bool bCached = itTotalDeriv->first.first == thisdofformat;
+        if( bCached ) {
+            if( IS_DEBUGLEVEL(Level_Verbose) ) {
                 RAVELOG_VERBOSE_FORMAT("Found cached derivatives of jointindex %d with respect to others", thisdofformat.jointindex);
             }
-            return bfound;
+
+            const int dependedJointIndex = itTotalDeriv->first.second;
+            const dReal partialDerivative = itTotalDeriv->second;
+            vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
+            return;
         }
-                                                                                            );
-    const bool bCached = mit != end(mTotalderivativepairValue);
-    if(bCached) {
-        for(const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal> &keyvalue : mTotalderivativepairValue) {
-            if(keyvalue.first.first == thisdofformat) {
-                const int dependedJointIndex = keyvalue.first.second;
-                const dReal partialDerivative = keyvalue.second;
-                vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
-            }
-        }
-        return;
     }
 
     /* ========== Collect values of joints on which joint z depends ========== */
@@ -2181,12 +2177,26 @@ void KinBody::Joint::_ComputePartialVelocities(std::vector<std::pair<int, dReal>
             vLocalIndexPartialPairs.clear();
             dependedjoint->_ComputePartialVelocities(vLocalIndexPartialPairs, iaxis, mTotalderivativepairValue); ///< recursion: computes ∂y/∂x
             for(const std::pair<int, dReal>& pIndexPartial : vLocalIndexPartialPairs) {
-                localmap[{thisdofformat, pIndexPartial.first}] += fvel * pIndexPartial.second; ///< dz/dx += ∂z/∂y * ∂y/∂x
+                std::pair<Mimic::DOFFormat, int> key = {thisdofformat, pIndexPartial.first};
+                ///< dz/dx += ∂z/∂y * ∂y/∂x
+                if( localmap.count(key) ) {
+                    localmap[key] += fvel * pIndexPartial.second;
+                }
+                else {
+                    localmap[key] = fvel * pIndexPartial.second;
+                }
             }
         }
         else {
             // depended joint is active
-            localmap[{thisdofformat, jointindex}] += fvel; ///< dz/dx += ∂z/∂x, as z may depend on others who depend on x
+            std::pair<Mimic::DOFFormat, int> key = {thisdofformat, jointindex};
+            ///< dz/dx += ∂z/∂x, as z may depend on others who depend on x
+            if( localmap.count(key) ) {
+                localmap[key] += fvel;
+            }
+            else {
+                localmap[key] = fvel;
+            }
         }
     }
 

src/libopenrave/kinbody.cpp
@@ -2736,7 +2736,7 @@ void KinBody::ComputeHessianTranslation(int linkindex, const Vector& position, s
                             RAVELOG_WARN("ComputeHessianTranslation joint %d not supported\n", pjoint->GetType());
                         }
                         PartialInfo& partialinfo = mappartialsinserted[vinsertedindices.size()];
-                        partialinfo.first.clear(); // resize(vinsertedindices.size());
+                        partialinfo.first.resize(vinsertedindices.size());
                         pjoint->_ComputePartialVelocities(partialinfo.second, idof, mapcachedpartials);
                         vinsertedindices.push_back(-1);
                     }
@@ -2951,7 +2951,7 @@ void KinBody::ComputeHessianAxisAngle(int linkindex, std::vector<dReal>& hessian
                             RAVELOG_WARN("ComputeHessianTranslation joint %d not supported\n", pjoint->GetType());
                         }
                         PartialInfo& partialinfo = mappartialsinserted[vinsertedindices.size()];
-                        partialinfo.first.clear(); // resize(vinsertedindices.size());
+                        partialinfo.first.resize(vinsertedindices.size());
                         pjoint->_ComputePartialVelocities(partialinfo.second, idof, mapcachedpartials);
                         vinsertedindices.push_back(-1);
                     }
@@ -3194,7 +3194,6 @@ void KinBody::ComputeInverseDynamics(std::vector<dReal>& doftorques, const std::
                 // TODO how to process this correctly? what is velocity of this joint? pjoint->GetVelocity(0)?
             }
 
-            vDofindexDerivativePairs.clear();
             pjoint->_ComputePartialVelocities(vDofindexDerivativePairs, 0, mapcachedpartials);
             for(const std::pair<int, dReal>& dofindexDerivativePair : vDofindexDerivativePairs) {
                 doftorques.at(dofindexDerivativePair.first) += dofindexDerivativePair.second * faxistorque;
@@ -3346,7 +3345,6 @@ void KinBody::ComputeInverseDynamics(boost::array< std::vector<dReal>, 3>& vDOFT
 
         bool bIsMimic = pjoint->GetDOFIndex() < 0 && pjoint->IsMimic(0);
         if( bIsMimic ) {
-            vpartials.clear();
             pjoint->_ComputePartialVelocities(vpartials, 0, mapcachedpartials);
         }
 
@@ -3787,7 +3785,7 @@ void KinBody::_ComputeInternalInformation()
 
     try {
         // initialize all the mimic equations
-        for(bool bPassiveJoints : {false, true}) {
+        for(int bPassiveJoints = 0; bPassiveJoints < 2; ++bPassiveJoints) { // simulate false/true
             const std::vector<JointPtr>& vjoints = bPassiveJoints ? _vPassiveJoints : _vecjoints;
             for(const JointPtr& pjoint : vjoints) {
                 const int ndof = pjoint->GetDOF();
@@ -3807,7 +3805,7 @@ void KinBody::_ComputeInternalInformation()
         // fill Mimic::_vmimicdofs, check that there are no circular dependencies between the mimic joints
         const int nActiveJoints = _vecjoints.size();
         std::map<Mimic::DOFFormat, MimicPtr> mapmimic; ///< collects if thisdofformat.jointaxis depends on a mimic joint
-        for(bool bPassiveJoints : {false, true}) {
+        for(int bPassiveJoints = 0; bPassiveJoints < 2; ++bPassiveJoints) { // simulate false/true
             const std::vector<JointPtr>& vjoints = bPassiveJoints ? _vPassiveJoints : _vecjoints;
             const int njoints = vjoints.size();
             for(int ijoint = 0; ijoint < njoints; ++ijoint) {
@@ -3835,7 +3833,7 @@ void KinBody::_ComputeInternalInformation()
                             const JointPtr pjointDepended = dofformat.GetJoint(*this);
                             if( pjointDepended->IsMimic(dofformat.axis) ) {
                                 mapmimic[thisdofformat] = pmimic; ///< pjoint depends on pjointDepended
-                                RAVELOG_DEBUG_FORMAT("mimic joint %s depends on mimic joint %s", pjoint->GetName() % pjointDepended->GetName());
+                                RAVELOG_VERBOSE_FORMAT("mimic joint %s depends on mimic joint %s", pjoint->GetName() % pjointDepended->GetName());
                                 break;
                             }
                         }
@@ -3888,7 +3886,7 @@ void KinBody::_ComputeInternalInformation()
     }
     catch(const std::exception& ex) {
         RAVELOG_ERROR(str(boost::format("failed to set mimic equations on kinematics body %s: %s\n")%GetName()%ex.what()));
-        for(bool bPassiveJoints : {false, true}) {
+        for(int bPassiveJoints = 0; bPassiveJoints < 2; ++bPassiveJoints) { // simulate false/true
             const std::vector<JointPtr>& vjoints = bPassiveJoints ? _vPassiveJoints : _vecjoints;
             for(const JointPtr& pjoint : vjoints) {
                 const int ndof = pjoint->GetDOF();

commit eea5cf905c8149e6b6116c37f1ef5ac9dcbc8c3f

fixed a bug introduced in 0537a3f3f5a585b70ceba1d4e911ae956a7787f8

src/libopenrave/kinbodyjoint.cpp
@@ -2126,16 +2126,25 @@ void KinBody::Joint::_ComputePartialVelocities(std::vector<std::pair<int, dReal>
         thisdofformat.jointindex = nActiveJoints + (std::find(begin(vPassiveJoints), end(vPassiveJoints), shared_from_this()) - begin(vPassiveJoints));
     }
 
+    bool bCached = false;
     for(const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal>& keyvalue : mTotalderivativepairValue) {
         if( keyvalue.first.first == thisdofformat ) {
             if( IS_DEBUGLEVEL(Level_Verbose) ) {
                 RAVELOG_VERBOSE_FORMAT("Found cached derivatives of jointindex %d with respect to others", thisdofformat.jointindex);
             }
-            const int dependedJointIndex = keyvalue.first.second;
-            const dReal partialDerivative = keyvalue.second;
-            vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
-            return;
+            bCached = true;
+            break;
         }
+    }
+    if(bCached) {
+        for(const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal> &keyvalue : mTotalderivativepairValue) {
+            if(keyvalue.first.first == thisdofformat) {
+                const int dependedJointIndex = keyvalue.first.second;
+                const dReal partialDerivative = keyvalue.second;
+                vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
+            }
+        }
+        return;
     }
 
     /* ========== Collect values of joints on which joint z depends ========== */