c6c76cefcc2316f8111fa0d30526673da5b188d3
rosen.diankov@gmail.com <rosen.diankov@mujin.co.jp>
2020-09-21 14:51:24 +0900

    remove unecessary second loop iteration

--- src/libopenrave/kinbodyjoint.cpp
+++ src/libopenrave/kinbodyjoint.cpp
@@ -2120,24 +2120,17 @@ void KinBody::Joint::_ComputePartialVelocities(std::vector<std::pair<int, dReal>
         thisdofformat.jointindex = nActiveJoints + (std::find(begin(vPassiveJoints), end(vPassiveJoints), shared_from_this()) - begin(vPassiveJoints));
     }
 
-    bool bCached = false;
     for(const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal>& keyvalue : mTotalderivativepairValue) {
         if( keyvalue.first.first == thisdofformat ) {
             if( IS_DEBUGLEVEL(Level_Verbose) ) {
                 RAVELOG_VERBOSE_FORMAT("Found cached derivatives of jointindex %d with respect to others", thisdofformat.jointindex);
             }
-            bCached = true;
-            break;
+            const int dependedJointIndex = keyvalue.first.second;
+            const dReal partialDerivative = keyvalue.second;
+            vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
         }
     }
-    if(bCached) {
-        for(const std::pair<const std::pair<Mimic::DOFFormat, int>, dReal> &keyvalue : mTotalderivativepairValue) {
-            if(keyvalue.first.first == thisdofformat) {
-                const int dependedJointIndex = keyvalue.first.second;
-                const dReal partialDerivative = keyvalue.second;
-                vDofindexDerivativePairs.emplace_back(dependedJointIndex, partialDerivative); // collect all dz/dx
-            }
-        }
+    if(!vDofindexDerivativePairs.empty()) {
         return;
     }
 
