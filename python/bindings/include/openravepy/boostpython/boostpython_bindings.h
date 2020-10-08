e4640ef82246b4ae3b642662b38fdf36007dacc6
Guangning Tan <guangning.tan@mujin.co.jp>
2020-10-08 10:38:23 +0900

    remove StdVectorToPyList

--- python/bindings/include/openravepy/boostpython/boostpython_bindings.h
+++ python/bindings/include/openravepy/boostpython/boostpython_bindings.h
@@ -226,16 +226,6 @@ struct int_from_number
     }
 };
 
-template <typename T>
-inline py::list StdVectorToPyList(const std::vector<T>& v) {
-    py::list l;
-    const size_t N = v.size();
-    for(size_t i = 0; i < N; i++) {
-        l.append(v[i]);
-    }
-    return l;
-};
-
 }
 
-#endif
+#endif // OPENRAVE_BOOSTPYTHON_BINDINGS_H
