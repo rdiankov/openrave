93666e38f3c51d705443a93fb8610c36311f85b6
Shintaro Matsuoka <shintaro.matsuoka@mujin.co.jp>
2021-02-05 13:30:31 +0900

    Add Python binding for Joint::IsActive()

--- python/bindings/include/openravepy/openravepy_jointinfo.h
+++ python/bindings/include/openravepy/openravepy_jointinfo.h
@@ -408,6 +408,7 @@ public:
     bool IsCircular(int iaxis) const;
     bool IsRevolute(int iaxis) const;
     bool IsPrismatic(int iaxis) const;
+    bool IsActive() const;
     bool IsStatic() const;
 
     int GetDOF() const;
