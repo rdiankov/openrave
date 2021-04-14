81a76f8bdc944ec36e151d64ada810a5bac94e8d
Kei Usui <kei.usui@mujin.co.jp>
2021-04-14 19:33:33 +0900

    use setter function to keep enable state mask of kin body in sync

--- src/libopenrave/kinbodystatesaver.cpp
+++ src/libopenrave/kinbodystatesaver.cpp
@@ -302,7 +302,7 @@ void KinBody::KinBodyStateSaverRef::_RestoreKinBody(KinBody& body)
         bool bchanged = false;
         for(size_t i = 0; i < _vEnabledLinks.size(); ++i) {
             if( body.GetLinks().at(i)->IsEnabled() != !!_vEnabledLinks[i] ) {
-                body.GetLinks().at(i)->_info._bIsEnabled = !!_vEnabledLinks[i];
+                body.GetLinks().at(i)->_Enable(!!_vEnabledLinks[i]);
                 bchanged = true;
             }
         }
