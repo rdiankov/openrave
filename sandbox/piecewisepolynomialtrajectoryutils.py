b3fd57409b06836f4bccba442667d25a9b32234e
Puttichai <puttichai.lertkultanon@mujin.co.jp>
2021-09-24 09:50:06 +0900

    updated plot fns for polynomials

--- sandbox/piecewisepolynomialtrajectoryutils.py
+++ sandbox/piecewisepolynomialtrajectoryutils.py
@@ -4,21 +4,24 @@ import numpy as np
 from pylab import ion
 ion()
 
-def PlotPolynomial(polynomial, tstart=None, tend=None, stepsize=0.01, derivative=0, fignum=None, **kwargs):
+def PlotPolynomial(polynomial, tstart=None, tend=None, stepsize=0.01, derivative=0, fignum=None, timeoffset=0, reusefig=False, **kwargs):
     """
     
     """
     if tstart is None and tend is None:
         tstart = 0
         tend = polynomial.duration
+    
     tVect = np.arange(tstart, tend, stepsize)
     if tVect[-1] < tend:
         tVect = np.append(tVect, tend)
-
     xVect = [polynomial.Evaldn(t, derivative) for t in tVect]
 
-    fig = plt.figure(fignum)
-    plt.plot(tVect, xVect, **kwargs)
+    fig = None
+    if not reusefig:
+        fig = plt.figure(fignum)
+    
+    plt.plot(tVect + timeoffset, xVect, **kwargs)
     plt.show(False)
     return fig
 
@@ -28,16 +31,8 @@ def PlotPiecewisePolynomial(pwpoly, stepsize=0.01, derivative=0, fignum=None, **
     fig = plt.figure(fignum)
     
     tstart = 0
-    polynomials = pwpoly.GetPolynomials()
-    for poly in polynomials:
-        tVect = np.arange(0, poly.duration, stepsize)
-        if tVect[-1] < poly.duration:
-            tVect = np.append(tVect, poly.duration)
-
-        xVect = [poly.Evaldn(t, derivative) for t in tVect]
-        tVect += tstart
-        plt.plot(tVect, xVect, **kwargs)
-
+    for poly in pwpoly.GetPolynomials():
+        PlotPolynomial(poly, stepsize=stepsize, derivative=derivative, timeoffset=tstart, reusefig=True, **kwargs)
         tstart += poly.duration
     plt.show(False)
     return fig
