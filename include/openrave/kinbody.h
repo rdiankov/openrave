b4f6d6a70f5d9fc56b472e5820c7f2cbb54e93f1
Puttichai <puttichai.lertkultanon@mujin.co.jp>
2021-12-16 10:37:08 +0900

    removed unused codes

--- include/openrave/kinbody.h
+++ include/openrave/kinbody.h
@@ -3478,52 +3478,6 @@ private:
     friend class Grabbed;
 };
 
-#if 0
-/// \brief The information of a currently grabbed body.
-class Grabbed : public UserData, public boost::enable_shared_from_this<Grabbed>
-{
-public:
-    Grabbed(KinBodyPtr pgrabbedbody, KinBody::LinkPtr plinkrobot) : _pgrabbedbody(pgrabbedbody), _plinkrobot(plinkrobot) {
-        _enablecallback = pgrabbedbody->RegisterChangeCallback(KinBody::Prop_LinkEnable, boost::bind(&Grabbed::UpdateCollidingLinks, this));
-        _plinkrobot->GetRigidlyAttachedLinks(_vattachedlinks);
-    }
-    virtual ~Grabbed() {
-    }
-    KinBodyWeakPtr _pgrabbedbody;         ///< the grabbed body
-    KinBody::LinkPtr _plinkrobot;         ///< robot link that is grabbing the body
-    std::list<KinBody::LinkConstPtr> _listNonCollidingLinks;         ///< links that are not colliding with the grabbed body at the time of Grab. Even if a link is disabled, it is considered as non-colliding as long as it is not colliiding with grabbed body
-    Transform _troot;         ///< root transform (of first link of body) relative to plinkrobot's transform. In other words, pbody->GetTransform() == plinkrobot->GetTransform()*troot
-    std::set<int> _setRobotLinksToIgnore; ///< original links of the robot to force ignoring
-
-    /// \brief check collision with all links to see which are valid.
-    ///
-    /// Use the robot's self-collision checker if possible
-    /// resets all cached data and re-evaluates the collisions
-    /// \param setRobotLinksToIgnore indices of the robot links to always ignore, in other words remove from non-colliding list
-    void ProcessCollidingLinks(const std::set<int>& setRobotLinksToIgnore);
-
-    inline const std::vector<KinBody::LinkPtr>& GetRigidlyAttachedLinks() const {
-        return _vattachedlinks;
-    }
-
-    void AddMoreIgnoreLinks(const std::set<int>& setRobotLinksToIgnore);
-
-    /// return -1 for unknown, 0 for no, 1 for yes
-    int WasLinkNonColliding(KinBody::LinkConstPtr plink) const;
-
-    /// \brief updates the non-colliding info while reusing the cache data from _ProcessCollidingLinks
-    ///
-    /// note that Regrab here is *very* dangerous since the robot could be a in a bad self-colliding state with the body. therefore, update the non-colliding state based on _mapLinkIsNonColliding
-    void UpdateCollidingLinks();
-
-private:
-    std::vector<KinBody::LinkPtr> _vattachedlinks;
-    UserDataPtr _enablecallback; ///< callback for grabbed body when it is enabled/disabled
-
-    std::map<KinBody::LinkConstPtr, int> _mapLinkIsNonColliding; // the collision state for each link at the time the body was grabbed.
-};
-#endif
-
 class Grabbed : public UserData, public boost::enable_shared_from_this<Grabbed>
 {
 public:
@@ -3582,8 +3536,7 @@ private:
     KinBody::KinBodyStateSaverPtr _pGrabberSaver; ///< statesaver that saves the snapshot of the grabber at the time Grab is called. The saved state will be used (i.e. restored) temporarily when computation of _listNonCollidingLinksWhenGrabbed is necessary.
     KinBody::KinBodyStateSaverPtr _pGrabbedSaver; ///< statesaver that saves the snapshot of the grabbed at the time Grab is called. The saved state will be used (i.e. restored) temporarily when computation of _listNonCollidingLinksWhenGrabbed is necessary.
 
-}; // end class MyGrabbed
-
+}; // end class Grabbed
 
 } // end namespace OpenRAVE
 
