889b79d01bdb4bbe19bfde25ee121f75f5b9a9b5
Ziyan <ziyan.zhou@mujin.co.jp>
2022-02-04 11:05:12 +0900

    Revert "InitFromInfo no longer sets id, instead manage id by SetId."
    This reverts commit d97b48c4c49a3ded5335c957780206df046d821b.

--- src/libopenrave-core/environment-core.h
+++ src/libopenrave-core/environment-core.h
@@ -2881,7 +2881,7 @@ public:
                         }
                     }
                     if (updateFromInfoResult != UFIR_NoChange && updateFromInfoResult != UFIR_Success) {
-                        // have to reinit, but do not SetId, keep id unchanged
+                        // have to reinit
                         if( !!pRobotBaseInfo ) {
                             pRobot->InitFromRobotInfo(*pRobotBaseInfo);
                         }
@@ -2899,7 +2899,7 @@ public:
                         updateFromInfoResult = pMatchExistingBody->UpdateFromKinBodyInfo(*pKinBodyInfo);
                     }
                     if (updateFromInfoResult != UFIR_NoChange && updateFromInfoResult != UFIR_Success) {
-                        // have to reinit, but do not SetId, keep id unchanged
+                        // have to reinit
                         pMatchExistingBody->InitFromKinBodyInfo(*pKinBodyInfo);
                     }
 
@@ -2918,11 +2918,9 @@ public:
                     }
 
                     if( !!pRobotBaseInfo ) {
-                        pRobot->SetId(pRobotBaseInfo->_id);
                         pRobot->InitFromRobotInfo(*pRobotBaseInfo);
                     }
                     else {
-                        pRobot->SetId(pKinBodyInfo->_id);
                         pRobot->InitFromKinBodyInfo(*pKinBodyInfo);
                     }
                     pInitBody = pRobot;
@@ -2935,7 +2933,6 @@ public:
                     if( !pNewBody ) {
                         pNewBody = RaveCreateKinBody(shared_from_this(), "");
                     }
-                    pNewBody->SetId(pKinBodyInfo->_id);
                     pNewBody->InitFromKinBodyInfo(*pKinBodyInfo);
                     pInitBody = pNewBody;
                     _AddKinBody(pNewBody, IAM_AllowRenaming);
