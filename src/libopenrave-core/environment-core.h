e1fa3c9213f5212c93f1397fce0851d62b139b16
Kei Usui <kei.usui@mujin.co.jp>
2022-02-09 12:28:19 +0900

    propagate body removal to self collision checker as well

--- src/libopenrave-core/environment-core.h
+++ src/libopenrave-core/environment-core.h
@@ -3223,6 +3223,12 @@ protected:
         if( !!_pCurrentChecker ) {
             _pCurrentChecker->RemoveKinBody(pbodyref);
         }
+        {
+            CollisionCheckerBasePtr pSelfColChecker = body.GetSelfCollisionChecker();
+            if (!!pSelfColChecker && pSelfColChecker != _pCurrentChecker) {
+                pSelfColChecker->RemoveKinBody(pbodyref);
+            }
+        }
         if( !!_pPhysicsEngine ) {
             _pPhysicsEngine->RemoveKinBody(pbodyref);
         }
