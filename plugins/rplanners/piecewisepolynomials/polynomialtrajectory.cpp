rdiankov/openrave

commit 71e82a1d034deb2cd919cf8d07d37fdc9a9b088f

fixed the interpolation polinomials classes

plugins/rplanners/piecewisepolynomials/polynomialtrajectory.cpp
@@ -796,9 +796,9 @@ void PiecewisePolynomial::CleanUp()
 //
 // Chunk
 //
-Chunk::Chunk(const dReal duration, const std::vector<Polynomial>& vpolynomials)
+Chunk::Chunk(const dReal duration_, const std::vector<Polynomial>& vpolynomials_)
 {
-    Initialize(duration, vpolynomials);
+    Initialize(duration_, vpolynomials_);
 }
 
 void Chunk::UpdateInitialValues(std::vector<dReal>&vinitialvalues)
@@ -818,15 +818,15 @@ void Chunk::UpdateDuration(dReal T)
     }
 }
 
-void Chunk::Initialize(const dReal duration, const std::vector<Polynomial>& vpolynomials)
+void Chunk::Initialize(const dReal duration_, const std::vector<Polynomial>& vpolynomials_)
 {
-    for( std::vector<Polynomial>::const_iterator itpoly = vpolynomials.begin(); itpoly != vpolynomials.end(); ++itpoly ) {
+    for( std::vector<Polynomial>::const_iterator itpoly = vpolynomials_.begin(); itpoly != vpolynomials_.end(); ++itpoly ) {
         if( !FuzzyEquals(itpoly->duration, duration, g_fPolynomialEpsilon) ) {
-            throw OPENRAVE_EXCEPTION_FORMAT("Polynomial index=%d has duration=%f (expected %f)", (itpoly - vpolynomials.begin())%(itpoly->duration)%duration, ORE_InvalidArguments);
+            throw OPENRAVE_EXCEPTION_FORMAT("Polynomial index=%d has duration=%f (expected %f)", (itpoly - vpolynomials_.begin())%(itpoly->duration)%duration, ORE_InvalidArguments);
         }
     }
-    this->duration = duration;
-    this->vpolynomials = vpolynomials;
+    this->duration = duration_;
+    this->vpolynomials = vpolynomials_;
     Initialize();
 }
 
@@ -929,31 +929,31 @@ void Chunk::Deserialize(std::istream& I)
     Initialize();
 }
 
-void Chunk::SetConstant(const std::vector<dReal>& x0Vect, const dReal duration, const size_t degree)
+void Chunk::SetConstant(const std::vector<dReal>& x0Vect, const dReal duration_, const size_t degree_)
 {
     this->dof = x0Vect.size();
-    this->degree = degree;
-    this->duration = duration;
+    this->degree = degree_;
+    this->duration = duration_;
     vpolynomials.resize(this->dof);
     std::vector<dReal> vcoeffs(this->degree + 1, 0);
     for( size_t idof = 0; idof < this->dof; ++idof ) {
         // All other coefficients are zero.
         vcoeffs[0] = x0Vect[idof];
-        vpolynomials[idof].Initialize(duration, vcoeffs);
+        vpolynomials[idof].Initialize(duration_, vcoeffs);
     }
 }
 
 //
 // Piecewise Polynomial Trajectory
 //
-PiecewisePolynomialTrajectory::PiecewisePolynomialTrajectory(const std::vector<Chunk>& vchunks)
+PiecewisePolynomialTrajectory::PiecewisePolynomialTrajectory(const std::vector<Chunk>& vchunks_)
 {
-    Initialize(vchunks);
+    Initialize(vchunks_);
 }
 
-void PiecewisePolynomialTrajectory::Initialize(const std::vector<Chunk>& vchunks)
+void PiecewisePolynomialTrajectory::Initialize(const std::vector<Chunk>& vchunks_)
 {
-    this->vchunks = vchunks;
+    this->vchunks = vchunks_;
     Initialize();
 }
 
@@ -1064,7 +1064,7 @@ void PiecewisePolynomialTrajectory::Deserialize(std::istream& I)
     Initialize();
 }
 
-void PiecewisePolynomialTrajectory::ReplaceSegment(dReal t0, dReal t1, const std::vector<Chunk>& vchunks)
+void PiecewisePolynomialTrajectory::ReplaceSegment(dReal t0, dReal t1, const std::vector<Chunk>& vchunks_)
 {
     OPENRAVE_ASSERT_OP(t0, <=, t1);
     size_t index0, index1;
@@ -1076,7 +1076,7 @@ void PiecewisePolynomialTrajectory::ReplaceSegment(dReal t0, dReal t1, const std
         this->vchunks[index0].Cut(rem1, tempChunk); // now tempChunk stores the portion from rem1 to duration
         this->vchunks.insert(this->vchunks.begin() + index0 + 1, tempChunk); // tempChunk is copied into the traj
         this->vchunks[index0].Cut(rem0, tempChunk); // now chunk index0 stores the portion from 0 to rem0
-        this->vchunks.insert(this->vchunks.begin() + index0 + 1, vchunks.begin(), vchunks.end()); // insert vchunks after index0
+        this->vchunks.insert(this->vchunks.begin() + index0 + 1, vchunks_.begin(), vchunks_.end()); // insert vchunks after index0
     }
     else {
         Chunk tempChunk;
@@ -1088,7 +1088,7 @@ void PiecewisePolynomialTrajectory::ReplaceSegment(dReal t0, dReal t1, const std
         this->vchunks[index0].Cut(rem0, tempChunk); // TODO: maybe define TrimBack function is better
 
         // Insert the input chunks before index1
-        this->vchunks.insert(this->vchunks.begin() + index1, vchunks.begin(), vchunks.end());
+        this->vchunks.insert(this->vchunks.begin() + index1, vchunks_.begin(), vchunks_.end());
 
         // Remove the previous segment from index0 to index1
         this->vchunks.erase(this->vchunks.begin() + index0 + 1, this->vchunks.begin() + index1);

plugins/rplanners/piecewisepolynomials/cubicinterpolator.cpp
@@ -17,19 +17,19 @@ namespace OpenRAVE {
 
 namespace PiecewisePolynomialsInternal {
 
-CubicInterpolator::CubicInterpolator(size_t ndof, int envid)
+CubicInterpolator::CubicInterpolator(size_t ndof_, int envid_)
 {
     __description = ":Interface Author: Puttichai Lertkultanon\n\nRoutines for cubic polynomial interpolation with specified boundary conditions.";
-    this->Initialize(ndof, envid);
+    this->Initialize(ndof_, envid_);
 }
 
-void CubicInterpolator::Initialize(size_t ndof, int envid)
+void CubicInterpolator::Initialize(size_t ndof_, int envid_)
 {
-    OPENRAVE_ASSERT_OP(ndof, >, 0);
-    this->ndof = ndof;
-    this->envid = envid;
-    _pGeneralInterpolator.reset(new GeneralRecursiveInterpolator(envid));
-    checker.Initialize(ndof, envid);
+    OPENRAVE_ASSERT_OP(ndof_, >, 0);
+    this->ndof = ndof_;
+    this->envid = envid_;
+    _pGeneralInterpolator.reset(new GeneralRecursiveInterpolator(envid_));
+    checker.Initialize(ndof_, envid_);
 
     // Jerk limits might be a few order of magnitude larger than velocity/acceleration
     // limits. Subsequently, when evaluating a value of acceleration (which is linear in the jerk
@@ -38,10 +38,10 @@ void CubicInterpolator::Initialize(size_t ndof, int envid)
     checker.SetEpsilonForAccelerationDiscrepancyChecking(100*g_fPolynomialEpsilon);
 
     _cache1DCoeffs.resize(4); // 4 coefficients for a cubic polynomial
-    _cacheDVect.resize(ndof);
-    _cachePolynomials.resize(ndof);
-    _cacheXVect.resize(ndof);
-    _cachePWPolynomials.resize(ndof);
+    _cacheDVect.resize(ndof_);
+    _cachePolynomials.resize(ndof_);
+    _cacheXVect.resize(ndof_);
+    _cachePWPolynomials.resize(ndof_);
 }
 
 //

plugins/rplanners/piecewisepolynomials/feasibilitychecker.h
@@ -19,7 +19,7 @@ namespace OpenRAVE {
 namespace PiecewisePolynomialsInternal {
 
 struct CheckReturn {
-    CheckReturn(int retcode=0, dReal fmult=1.0, dReal fvel=0, dReal faccel=0) : retcode(retcode), fTimeBasedSurpassMult(fmult), bDifferentVelocity(false), fMaxManipSpeed(fvel), fMaxManipAccel(faccel) {
+    CheckReturn(int retcode_=0, dReal fmult=1.0, dReal fvel=0, dReal faccel=0) : retcode(retcode_), fTimeBasedSurpassMult(fmult), bDifferentVelocity(false), fMaxManipSpeed(fvel), fMaxManipAccel(faccel) {
     }
 
     int retcode; ///< one of CFO_X defined in planner.h

plugins/rplanners/piecewisepolynomials/generalrecursiveinterpolator.cpp
@@ -21,19 +21,19 @@ namespace RampOptimizer = RampOptimizerInternal;
 
 namespace PiecewisePolynomialsInternal {
 
-GeneralRecursiveInterpolator::GeneralRecursiveInterpolator(int envid)
+GeneralRecursiveInterpolator::GeneralRecursiveInterpolator(int envid_)
 {
     __description = ":Interface Author: Puttichai Lertkultanon\n\nImplements polynomial interpolation routines. The implementation of Compute1DTrajectory function follows\n\n\
 Ezair, B., Tassa, T., & Shiller, Z. (2014). Planning high order trajectories with general initial and final conditions and asymmetric bounds. The International Journal of Robotics Research, 33(6), 898-916.\n\n\
 with some modifications to make the algorithm more robust.";
-    this->Initialize(envid);
+    this->Initialize(envid_);
 }
 
-void GeneralRecursiveInterpolator::Initialize(int envid)
+void GeneralRecursiveInterpolator::Initialize(int envid_)
 {
-    this->envid = envid;
-    checker.Initialize(1, envid);
-    parabolicInterpolator.Initialize(1, envid);
+    this->envid = envid_;
+    checker.Initialize(1, envid_);
+    parabolicInterpolator.Initialize(1, envid_);
     // Need to set a new epsilon for parabolic interpolator since its default value (g_fRampEpsilon
     // = 1e-10) is too loose compared epsilon = 1e-5*g_fPolynomialEpsilon used here.
     parabolicInterpolator.SetTolerance(epsilonFinalValidation);

plugins/rplanners/piecewisepolynomials/interpolatorbase.h
@@ -49,7 +49,7 @@ public:
     // Functions
     //
     /// \brief Initialize this interpolator
-    virtual void Initialize(size_t ndof, int envid=0)
+    virtual void Initialize(size_t ndof_, int envid_=0)
     {
         throw OPENRAVE_EXCEPTION_FORMAT0("Initialize not implemented", ORE_NotImplemented);
     }

plugins/rplanners/piecewisepolynomials/openravepy_piecewisepolynomials.cpp
@@ -133,15 +133,15 @@ public:
     {
         _ppolynomial.reset(new piecewisepolynomials::Polynomial());
     }
-    PyPolynomial(const dReal duration, const py::object ocoeffs)
+    PyPolynomial(const dReal duration_, const py::object ocoeffs_)
     {
-        std::vector<dReal> inputCoeffs = openravepy::ExtractArray<dReal>(ocoeffs);
-        _ppolynomial.reset(new piecewisepolynomials::Polynomial(duration, inputCoeffs));
+        std::vector<dReal> inputCoeffs = openravepy::ExtractArray<dReal>(ocoeffs_);
+        _ppolynomial.reset(new piecewisepolynomials::Polynomial(duration_, inputCoeffs));
         _PostProcess();
     }
-    PyPolynomial(const dReal duration, const std::vector<dReal>& coeffs)
+    PyPolynomial(const dReal duration_, const std::vector<dReal>& coeffs_)
     {
-        _ppolynomial.reset(new piecewisepolynomials::Polynomial(duration, coeffs));
+        _ppolynomial.reset(new piecewisepolynomials::Polynomial(duration_, coeffs_));
         _PostProcess();
     }
     PyPolynomial(const piecewisepolynomials::Polynomial& polynomial)
@@ -150,10 +150,10 @@ public:
         _PostProcess();
     }
 
-    void Initialize(const dReal duration, const py::object ocoeffs)
+    void Initialize(const dReal duration_, const py::object ocoeffs_)
     {
-        std::vector<dReal> inputCoeffs = openravepy::ExtractArray<dReal>(ocoeffs);
-        _ppolynomial->Initialize(duration, inputCoeffs);
+        std::vector<dReal> inputCoeffs = openravepy::ExtractArray<dReal>(ocoeffs_);
+        _ppolynomial->Initialize(duration_, inputCoeffs);
         _PostProcess();
     }
 
@@ -169,9 +169,9 @@ public:
         _PostProcess();
     }
 
-    void UpdateDuration(dReal duration)
+    void UpdateDuration(dReal duration_)
     {
-        _ppolynomial->UpdateDuration(duration);
+        _ppolynomial->UpdateDuration(duration_);
         _PostProcess();
     }
 
@@ -469,22 +469,22 @@ public:
         _pchunk.reset(new piecewisepolynomials::Chunk());
         _PostProcess();
     }
-    PyChunk(const dReal duration, const py::object ovpolynomials)
+    PyChunk(const dReal duration_, const py::object ovpolynomials)
     {
         std::vector<piecewisepolynomials::Polynomial> vpolynomials = ExtractArrayPolynomials(ovpolynomials);
-        _pchunk.reset(new piecewisepolynomials::Chunk(duration, vpolynomials));
+        _pchunk.reset(new piecewisepolynomials::Chunk(duration_, vpolynomials));
         _PostProcess();
     }
-    PyChunk(const dReal duration, const std::vector<piecewisepolynomials::Polynomial>& vpolynomials)
+    PyChunk(const dReal duration_, const std::vector<piecewisepolynomials::Polynomial>& vpolynomials)
     {
-        _pchunk.reset(new piecewisepolynomials::Chunk(duration, vpolynomials));
+        _pchunk.reset(new piecewisepolynomials::Chunk(duration_, vpolynomials));
         _PostProcess();
     }
 
-    void Initialize(const dReal duration, const py::object ovpolynomials)
+    void Initialize(const dReal duration_, const py::object ovpolynomials)
     {
         std::vector<piecewisepolynomials::Polynomial> vpolynomials = ExtractArrayPolynomials(ovpolynomials);
-        _pchunk->Initialize(duration, vpolynomials);
+        _pchunk->Initialize(duration_, vpolynomials);
         _PostProcess();
     }
 
@@ -495,9 +495,9 @@ public:
         _PostProcess();
     }
 
-    void UpdateDuration(const dReal duration)
+    void UpdateDuration(const dReal duration_)
     {
-        _pchunk->UpdateDuration(duration);
+        _pchunk->UpdateDuration(duration_);
         _PostProcess();
     }
 
@@ -547,10 +547,10 @@ public:
         return openravepy::toPyArray(res);
     }
 
-    void SetConstant(const py::object ox0Vect, const dReal duration, const size_t degree)
+    void SetConstant(const py::object ox0Vect, const dReal duration_, const size_t degree_)
     {
         std::vector<dReal> x0Vect = openravepy::ExtractArray<dReal>(ox0Vect);
-        _pchunk->SetConstant(x0Vect, duration, degree);
+        _pchunk->SetConstant(x0Vect, duration_, degree_);
         _PostProcess();
     }
 

plugins/rplanners/piecewisepolynomials/polynomialchecker.cpp
@@ -36,27 +36,24 @@ const char* GetPolynomialCheckReturnString(PolynomialCheckReturn ret)
     return "PCR(Unknown)";
 }
 
-PolynomialChecker::PolynomialChecker(size_t ndof, int envid)
+PolynomialChecker::PolynomialChecker(size_t ndof_, int envid_) : ndof(ndof_), envid(envid_)
 {
-    this->ndof = ndof;
-    this->envid = envid;
-
     _cacheCoordsVect.reserve(4); // quintic polynomial has at most 4 extrema
     _cacheXVect.resize(ndof);
     _cacheVVect.resize(ndof);
     _cacheAVect.resize(ndof);
 }
 
-void PolynomialChecker::Initialize(size_t ndof, int envid)
+void PolynomialChecker::Initialize(size_t ndof_, int envid_)
 {
-    this->ndof = ndof;
-    this->envid = envid;
+    this->ndof = ndof_;
+    this->envid = envid_;
     if( _cacheCoordsVect.capacity() < 4 ) {
         _cacheCoordsVect.reserve(4); // quintic polynomial has at most 4 extrema
     }
-    _cacheXVect.resize(ndof);
-    _cacheVVect.resize(ndof);
-    _cacheAVect.resize(ndof);
+    _cacheXVect.resize(ndof_);
+    _cacheVVect.resize(ndof_);
+    _cacheAVect.resize(ndof_);
 }
 
 PolynomialCheckReturn PolynomialChecker::CheckPolynomialValues(const Polynomial& p, const dReal t, const dReal x, const dReal v, const dReal a)

plugins/rplanners/piecewisepolynomials/quinticinterpolator.cpp
@@ -17,25 +17,25 @@ namespace OpenRAVE {
 
 namespace PiecewisePolynomialsInternal {
 
-QuinticInterpolator::QuinticInterpolator(size_t ndof, int envid)
+QuinticInterpolator::QuinticInterpolator(size_t ndof_, int envid_)
 {
     __description = ":Interface Author: Puttichai Lertkultanon\n\nRoutines for quintic polynomial interpolation with specified boundary conditions.";
     this->Initialize(ndof, envid);
 }
 
-void QuinticInterpolator::Initialize(size_t ndof, int envid)
+void QuinticInterpolator::Initialize(size_t ndof_, int envid_)
 {
-    OPENRAVE_ASSERT_OP(ndof, >, 0);
-    this->ndof = ndof;
-    this->envid = envid;
-    _pGeneralInterpolator.reset(new GeneralRecursiveInterpolator(envid));
-    checker.Initialize(ndof, envid);
+    OPENRAVE_ASSERT_OP(ndof_, >, 0);
+    this->ndof = ndof_;
+    this->envid = envid_;
+    _pGeneralInterpolator.reset(new GeneralRecursiveInterpolator(envid_));
+    checker.Initialize(ndof_, envid_);
     checker.SetEpsilonForJerkLimitsChecking(100*g_fPolynomialEpsilon);
 
     _cache1DCoeffs.resize(6);
 
-    _cacheDVect.resize(ndof);
-    _cachePolynomials.resize(ndof);
+    _cacheDVect.resize(ndof_);
+    _cachePolynomials.resize(ndof_);
 }
 
 //
@@ -284,4 +284,3 @@ PolynomialCheckReturn QuinticInterpolator::ComputeNDTrajectoryArbitraryTimeDeriv
 } // end namespace PiecewisePolynomialsInternal
 
 } // end namespace OpenRAVE
-

commit a5fb44ca974904e465e480facf1b9a82711dea90

fix bug introduced by 71e82a1d034deb2cd919cf8d07d37fdc9a9b088f

plugins/rplanners/piecewisepolynomials/polynomialtrajectory.cpp
@@ -821,7 +821,7 @@ void Chunk::UpdateDuration(dReal T)
 void Chunk::Initialize(const dReal duration_, const std::vector<Polynomial>& vpolynomials_)
 {
     for( std::vector<Polynomial>::const_iterator itpoly = vpolynomials_.begin(); itpoly != vpolynomials_.end(); ++itpoly ) {
-        if( !FuzzyEquals(itpoly->duration, duration, g_fPolynomialEpsilon) ) {
+        if( !FuzzyEquals(itpoly->duration, duration_, g_fPolynomialEpsilon) ) {
             throw OPENRAVE_EXCEPTION_FORMAT("Polynomial index=%d has duration=%f (expected %f)", (itpoly - vpolynomials_.begin())%(itpoly->duration)%duration, ORE_InvalidArguments);
         }
     }