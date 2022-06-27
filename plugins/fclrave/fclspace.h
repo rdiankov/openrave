eebf995e3f552a2665e9380a3705f3c45e544af9
Kei Usui <kei.usui@mujin.co.jp>
2022-06-27 18:18:53 +0900

    todo comment for the case of pgeom in collision report being nullptr

--- plugins/fclrave/fclspace.h
+++ plugins/fclrave/fclspace.h
@@ -276,6 +276,11 @@ public:
                     if( !pfclgeom ) {
                         continue;
                     }
+                    // because user data is null pointer here, in CheckNarrowPhaseGeomCollision report.pgeom ends up being null pointer and we lose the geometry information.
+                    // maybe pass plink->GetGeometries()[index] where index=distance(vgeometryinfos.begin(), itgeominfo)?
+                    // or, should the collision report store names of body, link, and geom?
+                    // also, currently there is no information about which geometry group was used for collision checking.
+                    // It's usually obvious immediately after CheckCollision is called, but later on, it it is not that obvious collision report is computed with which geometry group.
                     pfclgeom->setUserData(nullptr);
 
                     // We do not set the transformation here and leave it to _Synchronize
