f5250fb64bb5e5e1df680b732de16072c2f457a1
Kei Usui <kei.usui@mujin.co.jp>
2022-12-14 17:43:40 +0900

    bump version to invalidate previous cache

--- python/databases/convexdecomposition.py
+++ python/databases/convexdecomposition.py
@@ -134,7 +134,7 @@ class ConvexDecompositionModel(DatabaseGenerator):
         return self.linkgeometry is not None and len(self.linkgeometry)==len(self.robot.GetLinks())
     
     def getversion(self):
-        return 3
+        return 4
     
     def save(self):
         try:
