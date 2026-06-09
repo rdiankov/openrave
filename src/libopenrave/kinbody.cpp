b377eb71c5737ffeb4ecf77fa30213ffe748d699
Shunichi Nozawa <shunichi.nozawa@mujin.co.jp>
2026-06-09 18:00:53 +0900

    Remove comment

--- src/libopenrave/kinbody.cpp
+++ src/libopenrave/kinbody.cpp
@@ -6424,7 +6424,6 @@ void KinBody::_InitAndAddJoint(JointPtr pjoint)
 
 void KinBody::ExtractInfo(KinBodyInfo& info, ExtractInfoOptions options)
 {
-    RAVELOG_DEBUG_FORMAT("ExtractInfo %s", _name);
     info._modifiedFields = 0;
     info._id = _id;
     info._uri = GetURI();
@@ -6547,7 +6546,6 @@ void KinBody::ExtractInfo(KinBodyInfo& info, ExtractInfoOptions options)
         }
         info._prAssociatedFileEntries->CopyFrom(*_prAssociatedFileEntries, info._prAssociatedFileEntries->GetAllocator());
     }
-    RAVELOG_DEBUG_FORMAT("ExtractInfo done %s", _name);
 }
 
 UpdateFromInfoResult KinBody::UpdateFromKinBodyInfo(const KinBodyInfo& info)
