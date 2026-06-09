503186a7bbe0863f0acacdfd7a196eff0c326997
Puttichai <puttichai.lertkultanon@mujin.co.jp>
2026-06-09 18:22:20 +0900

    style(kinbody): reword link-transform store comments in SetDOFValues
    (cherry picked from commit a5b3bf5cb2964d1db129b2ed9ef543d3175d043f)

--- src/libopenrave/kinbody.cpp
+++ src/libopenrave/kinbody.cpp
@@ -2386,7 +2386,7 @@ void KinBody::SetDOFValues(const dReal* pJointValues, int dof, uint32_t checklim
         if( joint.IsStatic() ) {
             // if joint.IsStatic(), then joint._info._tRightNoOffset and tjoint are assigned identities
             const Transform t = (!!parentlink ? parentlink->GetTransform() : _veclinks.at(0)->GetTransform()) * joint.GetInternalHierarchyLeftTransform();
-            // O1: store the transform directly instead of childlink->SetTransform(t), which would lock the parent
+            // store the transform directly instead of childlink->SetTransform(t), which would lock the parent
             // weak_ptr and bump the update stamp once per link. The stamp is bumped once in _PostprocessChangedParameters below.
             childlink->_info._t = t;
             vlinkscomputed[childlink->GetIndex()] = 1;
@@ -2572,7 +2572,7 @@ void KinBody::SetDOFValues(const dReal* pJointValues, int dof, uint32_t checklim
         }
 
         const Transform t = (!!parentlink ? parentlink->GetTransform() : _veclinks.at(0)->GetTransform()) * (joint.GetInternalHierarchyLeftTransform() * tjoint * joint.GetInternalHierarchyRightTransform());
-        // O1: store directly (see note above) instead of childlink->SetTransform(t) to avoid the per-link weak_ptr lock + stamp bump.
+        // store directly (see note above) instead of childlink->SetTransform(t) to avoid the per-link weak_ptr lock + stamp bump.
         childlink->_info._t = t;
         vlinkscomputed[childlink->GetIndex()] = 1;
     }
