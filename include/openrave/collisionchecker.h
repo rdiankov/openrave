7cc54807c6ed2d87914f0909000703c774176634
Shunichi Nozawa <shunichi.nozawa@mujin.co.jp>
2026-06-30 19:30:00 +0900

    remove vector version

--- include/openrave/collisionchecker.h
+++ include/openrave/collisionchecker.h
@@ -400,12 +400,6 @@ public:
     /// \parma[in] required : If options are not supported and required is true, throws an exception.
     CollisionOptionsStateSaverAll(const EnvironmentBasePtr pEnv, const int options, const bool required=true, const CollisionOptionsModificationType modificationType=COMT_Add);
 
-    /// \brief Constructor
-    /// \param[in] pEnv : Environment
-    /// \param[in] vOptions : vector of options. size should be same as pEnv->GetCollisionCheckers(). order should be consistent with it.
-    /// \parma[in] required : If options are not supported and required is true, throws an exception.
-    CollisionOptionsStateSaverAll(const EnvironmentBasePtr pEnv, const std::vector<int>& vOptions, const bool required=true);
-
     virtual ~CollisionOptionsStateSaverAll();
 private:
     /// \brief restore the options. Assume _vOldOptions and _vCheckers have same sizes.
